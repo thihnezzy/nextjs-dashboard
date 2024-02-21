46fd92e1cccbae309fec74f234f4a971302e4da1
Quoc Thinh <qnguyen@noota.io>
2024-02-21 22:41:34 +0100

    feat: adding files

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default function Page(): React.ReactElement {
-    return <p>Dashboard Page</p>;
+    return <p>
+      <div className="bg-[#f0f0f0] w-fit px-4 py-1 rounded-lg">
+        AI
+      </div>
+      Dashboard Page
+    </p>;
   }
